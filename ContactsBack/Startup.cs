394f9fd950f9a2a75f54bd079da0005cdbf5e7a5
ldalzate09 <diana.alzate@gmail.com>
2017-06-02 19:55:43 -0500

    Version Inicial

---
+++ ContactsBack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactsBack.Startup))]
+namespace ContactsBack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
